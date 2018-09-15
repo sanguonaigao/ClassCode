2c46de603733a85d5c9195cf60d5d3a7337c57c5
张鹏伟 <zpengwei@yeah.net>
2018-09-15 17:30:05 +0800

    添加课代码

---
+++ 51class/test_9_14/test_9_14/test.c
@@ -0,0 +1,186 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+/*
+int main()
+{
+	//c:\code\test10\test.c
+	//转移字符
+	//printf("%s", "c:\\code\\test10\\test.c");
+	//printf("%s\n", "((are you ok\?\?))");//三字母词
+	//printf("%c\n", '\'');
+	//printf("%s", "\"");
+	//printf("\a\a\a\a\a\a\a\a\a");
+	//printf("%d\n", strlen("c:\test\32\test.c"));
+	/*
+	printf("%c\n", '\x040');//64
+	*/
+	//((are you ok??))
+	return 0;
+}
+*/
+
+//int main()
+//{
+//	//abcdef;
+//	char arr1[] = "abcdef";
+//
+//	char arr2[] = {'a', 'b', 'c', 'd', 'e', 'f', '\0'};
+//	printf("%s\n", "abcdef");//
+//	printf("%s\n", arr1);//
+//	printf("%s\n", arr2);//
+//	
+//	return 0;
+//}
+//
+////#define定义的标识符常量
+//#define  MAX 1000
+//
+//enum Sex
+//{
+//	//枚举常量
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	enum Sex sex = MALE;
+//	printf("%d\n", MALE);//
+//	printf("%d\n", FEMALE);//
+//	printf("%d\n", SECRET);//
+//
+//	//MAX = 2000;
+//	//printf("%d\n", MAX);//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//const int num = 10;//常量
+//	//num = 20;
+//	//const int n = 10;
+//	//int arr[n] = {0};
+//	//3;
+//	//3.14;
+//	//3 = 5;
+//	return 0;
+//}
+
+// int main()
+// {
+// 	printf("hehe\n");
+// 
+// 	system("pause");
+// 	return 0;
+// }
+
+
+// #include <stdio.h>
+// 
+// int main()
+// {
+// 	printf("age = %d\n", age);
+// 	{
+// 		int age = 10;//
+// 	}
+// 	printf("age = %d\n", age);//???
+// 	return 0;
+// }
+
+//
+//int g_val = 100;
+//
+//void print()
+//{
+//	printf("----%d----\n", g_val);//4
+//}
+//int main()
+//{
+//	{
+//		int age = 20;
+//		{
+//			printf("1: %d\n", g_val);//1
+//		}
+//		printf("2: %d\n", g_val);//2
+//	}
+//	printf("3: %d\n", g_val);//3
+//	print();
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1+num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//代码块
+//{}
+//全局变量
+//int age = 20;
+//
+//int main()
+//{
+//	//局部变量
+//	int age = 10;
+//	{
+//		int age = 30;
+//		printf("age = %d\n", age);
+//	}
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	float weight = 45.5f;//double
+//// 	int age = 20;
+//// 	printf("%d\n", age);
+//// 	age = 21;
+//// 	printf("%d\n", age);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(char));//1
+//	printf("%d\n", sizeof(short));//2
+//	printf("%d\n", sizeof(int));//4
+//	printf("%d\n", sizeof(long));//4
+//	printf("%d\n", sizeof(long long));//8
+//	printf("%d\n", sizeof(float));//4
+//	printf("%d\n", sizeof(double));//8
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 10;
+//	scanf("%d", &num);
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//
+////程序的入口
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	return 0;
+//}
---
+++ 51class/test_9_14/test_9_14/test2.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//
+////程序的入口
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	return 0;
+//}
