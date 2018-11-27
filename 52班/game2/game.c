0185743e94eed44af24188fb1c54a887b5aa562e
张鹏伟 <zpengwei@yeah.net>
2018-11-27 21:35:59 +0800

    更新代码

---
+++ 52班/game2/game.c
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "game.h"
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set)
+{
+	int i = 0;
+	int j = 0;
+	for(i=0; i<rows; i++)
+	{
+		for(j=0; j<cols; j++)
+		{
+			board[i][j] = set;
+		}
+	}
+}
+
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	printf("------------------------------\n");
+	for(i=0; i<=col; i++)
+	{
+		printf("%d ", i);
+	}
+	printf("\n");
+	for(i=1; i<=row; i++)
+	{
+		printf("%d ", i);
+		for(j=1; j<=col; j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+	printf("------------------------------\n");
+}
+
+void SetMine(char mine[ROWS][COLS], int row, int col)
+{
+	int count = EASY_COUNT;
+	int x = 0;
+	int y = 0;
+
+	while(count)
+	{
+		x = rand()%row+1;
+		y = rand()%col+1;
+		if(mine[x][y] == '0')
+		{
+			mine[x][y] = '1';
+			count--;
+		}
+	}
+}
+
+
+int GetMineCount(char mine[ROWS][COLS], int x, int y)
+{
+	return mine[x-1][y]+
+	mine[x-1][y-1]+
+	mine[x][y-1]+
+	mine[x+1][y-1]+
+	mine[x+1][y]+
+	mine[x+1][y+1]+
+	mine[x][y+1]+
+	mine[x-1][y+1] - 8*'0';
+}
+
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], 
+			  int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int win = 0;
+
+	while(win<row*col-EASY_COUNT)
+	{
+		printf("请输入要排查的坐标:>");
+		scanf("%d%d", &x, &y);
+		if(x>=1 && x<=row && y>=1 && y<=col)
+		{
+			if(mine[x][y] == '1')
+			{
+				printf("很遗憾，你被炸死了\n");
+				DisplayBoard(mine, row, col);
+				break;
+			}
+			else
+			{
+				//统计x,y坐标周围有几个雷
+				int count = GetMineCount(mine, x, y);
+				show[x][y] = count+'0';
+				DisplayBoard(show, row, col);
+				win++;
+			}
+		}
+		else
+		{
+			printf("输入坐标有误\n");
+		}
+	}
+	if(win == row*col-EASY_COUNT)
+	{
+		printf("恭喜你，排雷成功\n");
+		DisplayBoard(mine, row, col);
+	}
+}
