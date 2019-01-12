2a33c3fbae1f57f167e7831f96ad9f9104daa43d
张鹏伟 <zpengwei@yeah.net>
2019-01-12 12:50:52 +0800

    更新代码

--- 52班/test_1_11/test_1_11/test.c
+++ 52班/test_1_11/test_1_11/test.c
@@ -3,31 +3,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct S
-{
-	char name[20];
-	int age;
-	double d;
-};
-
-
-int main()
-{
-	FILE* pf = fopen("test.txt", "rb");
-	struct S s = {0};
-	if(pf == NULL)
-	{
-		perror("fopen");
-		return 0;
-	}
-	//写文件
-	fread(&s, sizeof(struct S), 1, pf);
-	printf("%s %d %lf\n", s.name, s.age, s.d);
-	fclose(pf);
-	pf = NULL;
-	system("pause");
-	return 0;
-}
+//
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	double d;
+//};
+//
+//
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "rb");
+//	struct S s = {0};
+//	if(pf == NULL)
+//	{
+//		perror("fopen");
+//		return 0;
+//	}
+//	//写文件
+//	fread(&s, sizeof(struct S), 1, pf);
+//	printf("%s %d %lf\n", s.name, s.age, s.d);
+//	fclose(pf);
+//	pf = NULL;
+//	system("pause");
+//	return 0;
+//}
 //
 //int main()
 //{
---
+++ 52班/test_1_12/test_1_12/test.c
@@ -0,0 +1,111 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+// 
+// #define SQUARE(X) ((X)*(X))
+// int Sqaure(int x)
+// {
+// 	return x*x;
+// }
+// #define MAX 10
+// int main()
+// {
+// 	printf("%d\n", SQUARE(MAX+4));
+// 	printf("%d\n", Sqaure(5+4));
+// 	printf("MAX = %d\n", 10);
+// 	//printf("%d\n", 5+4*5+4);
+// 	system("pause");
+// 	return 0;
+// }
+//#define MAX 1000
+
+// int main()
+// {
+// 	//printf("%d\n", MAX);
+// 	int a = MAX;
+// 	return 0;
+// }
+//#define MAX 1000
+//
+//#define CASE break;case
+//int main()
+//{
+//	int age = 0;
+//	switch(age)
+//	{
+//	case 1:
+//	CASE 2:
+//	CASE 3:
+//	}
+//	return 0;
+//}
+
+// 
+// int main()
+// {
+// 	printf("%d\n", MAX);
+// 	//printf("%s\n", __FUNCTION__);
+// 	system("pause");
+// 	return 0;
+// }
+//
+//int main()
+//{
+//	int i = 0;
+//	printf("%d\n", __STDC__);
+//	//FILE* pf = fopen("log.txt", "w");
+//// 	for(i=0; i<10; i++)
+//// 	{
+//// 		fprintf(pf, "file:%s line=%d date=%s time=%s i=%d\n", \
+//// 			__FILE__, __LINE__, __DATE__, __TIME__, i);
+//// 	}
+//	//fclose(pf);
+//	//pf = NULL;
+//	system("pause");
+//	return 0;
+//}
+//IDE
+//集成开发环境
+//
+//extern int Add(int x, int y);
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int ret = Add(a, b);
+//	printf("%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//cd change directory
+//eg: cd /
+//pwd print work directory
+//pwd
+//ls  -list
+//..  上一级目录
+//cd ..
+//rm remove
+//rm test.c
+//rm -f test.c
+//rm -rf *
+//mv -move
+//mkdir   make directory
+//
+//vim-- 编辑器
+//vim test.c
+//按 i  进入插入模式  >>> ESC  >>> 普通模式 >> shift + : 命令行模式(:w :q :wq)
+//gcc -- 编译器
+//gcc test.c
+//gcc test.c -o test52
+//生成了a.out文件--可执行程序
+//
+
+
+
+
+
+
+
