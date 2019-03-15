c3e742cabff540cade91f6a5c15fd8f331af728d
张鹏伟 <zpengwei@yeah.net>
2019-03-15 21:38:46 +0800

    更新上课代码

---
+++ 60班/test_3_15/test_3_15/sum.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//int  main()
+//{
+//	printf("hehe\n");
+//	return 0;
+//}
---
+++ 60班/test_3_15/test_3_15/test.c
@@ -0,0 +1,231 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+//
+//int main()
+//{
+//	//int a = 10;//C++的注释风格
+//	/*
+//	int b = 20;
+//	*/
+//	//C语言的注释风格
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	//printf("%d\n", strlen("c:\test\328\test.c"));
+//	//printf("%c\n", '\130');//64+24
+//	printf("%c\n", '\x050');
+//	//123
+//	//070 - 56
+//	//@!w$%
+//	//a-97
+//	//A-65
+//	//ASCII 编码
+//	//
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	printf("\a\a\a\a\a\a\a");
+//	system("pause");
+//	return 0;
+//}
+// 
+// int main()
+// {
+// 	printf("c:\\test\\test.c");
+// 	//printf("hehe\n");
+// 	//printf("%c\n", '\'');
+// 	//printf("%s\n", "\"");
+// 	system("pause");
+// 	return 0;
+// }
+
+//
+//int main()
+//{
+//	int len = strlen("abcd");
+//	printf("%d\n", len);//string length
+//	//char arr1[] = "hehe";
+//	//char arr2[] = {'h', 'e', 'h', 'e', '\0'};
+//	//printf("%s\n", "hehe");//hehe
+//	//printf("%s\n", arr1);//hehe
+//	//printf("%s\n", arr2);//hehe
+//
+//	system("pause");
+//	return 0;
+//}
+//
+//struct Stu
+//{
+//	char name[10];
+//	int age;
+//};
+//
+////枚举常量
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	//enum Sex s = MALE;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRET);
+//	system("pause");
+//	return 0;
+//}
+
+
+//#define 定义的标识符常量
+//#define MAX 1000
+//int main()
+//{
+//	int a =	MAX;
+//	MAX = 10000;
+//	printf("a = %d\n", a);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//3;
+//	//3.14;
+//	//const int num = 10;
+//	//num = 20;
+//	//printf("%d\n", num);
+//	//const int n = 10;
+//	//int arr[n] = {0};
+//
+//	system("pause");
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	{
+//		int a = 10;
+//	}
+//	return 0;
+//}
+
+//声明
+//extern int g_val;
+//
+//int main()
+//{
+//	//printf("%d\n", g_val);
+//	system("pause");
+//	return 0;
+//}
+
+//int a = 100;
+//
+//int main()
+//{
+//	printf("a = %d\n", a);
+//	{
+//		printf("a = %d\n", a);	
+//	}
+//	printf("a = %d\n", a);
+//	system("pause");
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("%d\n", sum);
+//	system("pause");
+//	return 0;
+//}
+
+//
+//int a = 20;//全局变量
+////代码块 -{}
+//
+//int main()
+//{
+//	int a = 10;//局部变量
+//	printf("a = %d\n", a);//10
+//	system("pause");
+//	return 0;
+//}
+// int main()
+// {
+// 	float weight = 0.0;
+// 	char ch = 'w';
+// 
+// 	return 0;
+// }
+//
+//int main()
+//{
+//	//char short int long ....
+//	printf("%d\n", sizeof(char));//1 字节
+//	printf("%d\n", sizeof(short));//2 4
+//	printf("%d\n", sizeof(int));//4
+//	printf("%d\n", sizeof(long));//8 
+//	printf("%d\n", sizeof(long long));//16 8
+//	printf("%d\n", sizeof(float));//8 4
+//	printf("%d\n", sizeof(double));//16 8
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//Centos 7.0
+//unbuntu
+//redhat
+
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	printf("age = %d\n", age);
+//	return 0;
+//}
+// 
+// //主函数-程序的入口
+// int main()
+// {
+// 	//输出函数
+// 	printf("hehe\n");
+// 	printf("hehe\n");
+// 	printf("hehe\n");
+// 	printf("hehe\n");
+// 	printf("hehe\n");
+// 
+// 	//暂停
+// 	system("pause");
+// 	return 0;
+// }
