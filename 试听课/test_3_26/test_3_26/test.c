6cc186cdba404d4ddcb5daa3fb6b10f0851a8e25
张鹏伟 <zpengwei@yeah.net>
2019-03-26 21:52:56 +0800

    更新代码

---
+++ 试听课/test_3_26/test_3_26/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//标准输入输出头文件
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//	scanf("%d", &money);//20
+//	/*total = money;
+//	empty = money;
+//	while(empty>=2)
+//	{
+//	total = total + empty/2;
+//	empty = empty/2+empty%2;
+//	}*/
+//	total = money*2-1;
+//	printf("total = %d\n", total);
+//	system("pause");
+//	return 0;
+//}
+//
+////主函数-程序入口
+//int main()
+//{
+//	//输出函数
+//	printf("hehe\n");
+//
+//	//暂停
+//	system("pause");
+//	return 0;
+//}
+//
+
