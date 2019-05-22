fa349c2f4f49500960ed89ae8c9cd3b5e3a08bc2
张鹏伟 <zpengwei@yeah.net>
2019-05-22 21:37:10 +0800

    试听课的代码

---
+++ 试听课/test_5_22/test_5_22/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+//
+//int main()
+//{
+//	printf("hehe\n");
+//	//暂停
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//	scanf("%d", &money);//20
+//
+//	total = money*2-1;
+//
+//	/*total = money;
+//	empty = money;
+//	while(empty>=2)
+//	{
+//	total = total + empty/2;
+//	empty = empty/2 + empty%2;
+//	}*/
+//	printf("total = %d\n", total);
+//	system("pause");
+//	return 0;
+//}
+//
+
+#include <string.h>
+
+//int main()
+//{
+//	char input[20] = {0};
+//	system("shutdown -s -t 60");
+//again:
+//	printf("请注意，你的电脑在1分钟内关机，如果输入:我是猪，就取消关机\n");
+//	scanf("%s", input);
+//	if(strcmp(input, "我是猪") == 0)
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
+//
+
+
+
