1da55eed39254104037a299b795b87af8ba191ee
张鹏伟 <zpengwei@yeah.net>
2019-08-13 14:52:14 +0800

    跟新代码

---
+++ 81班/test_8_13_作业/test_8_13_作业/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+// 
+// int main()
+// {
+// 	int arr1[] = {1,3,5,7,9};
+// 	int arr2[] = {2,4,6,8,0};
+// 	int sz = sizeof(arr1)/sizeof(arr1[0]);
+// 	int i = 0;
+// 	for(i=0; i<sz; i++)
+// 	{
+// 		int tmp = arr1[i];
+// 		arr1[i] = arr2[i];
+// 		arr2[i] = tmp;
+// 	}
+// 	for(i=0; i<sz; i++)
+// 	{
+// 		printf("%d ", arr1[i]);
+// 	}
+// 	printf("\n");
+// 	for(i=0; i<sz; i++)
+// 	{
+// 		printf("%d ", arr2[i]);
+// 	}
+// 	printf("\n");
+// 	return 0;
+// }
+//
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//
+//	for(i=1; i<=100; i++)
+//	{
+//		sum += flag*1.0/i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//
+//	for(i=1; i<=100; i++)
+//	{
+//		if(i%10==9)
+//			count++;
+//		if(i/10==9)
+//			count++;
+//	}
+//	printf("%d\n", count);
+//
+//	return 0;
+//}
+//
