f653e8f6f9dcaacaf3313cc816568be0e22f0650
张鹏伟 <zpengwei@yeah.net>
2019-09-10 20:28:04 +0800

    更新56班试听课代码

---
+++ 试听课/test_9_10/test_9_10/test.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//库函数
+#include <stdio.h>
+//
+//int main()
+//{
+//	//printf("hello world\n");
+//	//INT_MAX;
+//	int num1 = 3;//
+//	int num2 = 5;
+//	int avg = num1 + (num2-num1)/2;
+//
+//	/*int num1 = 3;
+//	int num2 = 5;
+//	int avg = num1/2+num2/2;*/
+//
+//	/*int avg = (num1+num2)/2;*/
+//	//3 5 4
+//	//3/2=1   5/2=2
+//
+//	printf("%d\n", avg);//15
+//	return 0;
+//}
+#include <stdlib.h>
+#include <string.h>
+//
+//int main()
+//{
+//	//库函数
+//	char input[20] = {0};
+//	system("shutdown -s -t 60");
+//again:
+//	printf("电脑在1分钟内关机，如果输入：我是猪，就取消关机\n");
+//	scanf("%s", input);
+//	if(strcmp(input, "我是猪") == 0)
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = x+y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	//函数
+//	int sum = Add(num1, num2);
+//
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//
+
