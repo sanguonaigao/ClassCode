7e46445f33beba09e33d7577e875daa44ed2a33b
张鹏伟 <zpengwei@yeah.net>
2019-09-19 22:59:10 +0800

    财院宣传讲课

---
+++ 试听课/test_9_19/test_9_19/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+//main-主函数-程序的入口
+//
+//int main()
+//{
+//	//输出库函数
+//	printf("hello world\n");
+//	printf("hello world\n");
+//	printf("hello world\n");
+//	printf("hello world\n");
+//
+//	return 0;
+//}
+//
+
+//
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//	scanf("%d", &money);//20
+//	total = money*2-1;
+//
+//	//total = money;
+//	//empty = money;
+//	////置换
+//	//while(empty>=2)
+//	//{
+//	//	total += empty/2;
+//	//	empty = empty/2 + empty%2;
+//	//}
+//	printf("total = %d\n", total);
+//
+//	return 0;
+//}
+
+int main}()
+{
+
+	return 0;
+}
