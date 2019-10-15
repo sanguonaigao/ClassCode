3a10c6663e07424444526c14d17be1ced347f4c4
张鹏伟 <zpengwei@yeah.net>
2019-10-15 22:17:02 +0800

    上课理学院讲座代码更新

---
+++ 试听课/test_10_15/test_10_15/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	char input[20] = {0};
+	system("shutdown -s -t 60");
+again:	
+	printf("请注意你的电脑在1分钟内关机,如果输入：我是猪，就取消关机\n");
+	scanf("%s", input);
+	if(strcmp(input, "我是猪") == 0)
+	{
+		system("shutdown -a");
+	}
+	else
+	{
+		goto again;
+	}
+	return 0;
+}
+
+
