8a5ea343563f40ccf2815302a839a2768437e178
张鹏伟 <zpengwei@yeah.net>
2019-11-27 23:00:49 +0800

    更新代码

--- 试听课/test_11_21_2/test_11_21_2/test.c
+++ 试听课/test_11_21_2/test_11_21_2/test.c
@@ -63,4 +63,32 @@
 #include <stdio.h>
 #include <string.h>
 #include<stdio.h>
+//int main()
+//{
+//	unsigned char puc[4];
+//	struct tagPIM
+//	{
+//		unsigned char ucPim1;
+//		unsigned char ucData0 : 1;
+//		unsigned char ucData1 : 2;
+//		unsigned char ucData2 : 3;
+//	}*pstPimData;
+//	pstPimData = (struct tagPIM*)puc;
+//	memset(puc,0,4);
+//	pstPimData->ucPim1 = 2; 
+//	pstPimData->ucData0 = 3;
+//	pstPimData->ucData1 = 4;
+//	pstPimData->ucData2 = 5;
+//	printf("%02x %02x %02x %02x\n",puc[0], puc[1], puc[2], puc[3]);
+//	return 0;
+//}
 
+#include <stdio.h>
+int main()
+{
+	int a = 0x11223344;
+	char *pc = (char*)&a;
+	*pc = 0;
+	printf("%x\n", a);
+	return 0;
+}
